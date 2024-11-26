344fac67f9758b436d281919f09587a1ecc87774
Chris Eldon <chris_eldon@yahoo.com>
2024-11-27 00:56:13 +0700

    fix: remove keep alive (bad practice)

--- src/app/api/process/route.ts
+++ src/app/api/process/route.ts
@@ -14,8 +14,6 @@ export async function POST(req: NextRequest) {
     method: 'POST',
     headers: {
       'Authorization': `Bearer ${token.accessToken}`,
-      'Connection': 'keep-alive', 
-      'Keep-Alive': 'timeout=0, max=0'
     },
     body: formData,
   })
